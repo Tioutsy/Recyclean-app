ee0062fe9169d70a4f0e38dd35edd9fe614ff3d1
Tioutsy <79256642+Tioutsy@users.noreply.github.com>
2026-06-29 16:57:38 +0400

    Update db.js

--- db.js
+++ db.js
@@ -4,9 +4,10 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl:
+    process.env.DATABASE_URL?.includes("sslmode=require")
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
